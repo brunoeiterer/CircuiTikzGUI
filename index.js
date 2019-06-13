c08c1727a9006203f6dbeeaea8aaf49137054a2d
unknown <brunoeiterer@gmail.com>
2019-06-13 18:18:10 -0300

    Add snap to grid on component dragging

--- index.js
+++ index.js
@@ -19,11 +19,26 @@ function OnComponentClick(img) {
     newComponent.src = img.src;
     newComponent.className = "component"
     newComponent.addEventListener("dragstart", ElementDragStart);
+    newComponent.addEventListener("drag", ElementDrag);
+    newComponent.addEventListener("dragend", ElementDragEnd);
     document.getElementById("circuit-container").appendChild(newComponent);
 }
 
 function ElementDragStart(event) {
+    /* store the target id in the data transfer object */
     event.dataTransfer.setData("text/plain", event.target.id);
+
+    /* store the target id in the local storage for events that can't access the data transfer object */
+    localStorage.setItem("draggedElement", event.target.id);
+
+    /* remove the ghost image when dragging */
+    var ghostImage = new Image();
+    ghostImage.src = "images/blank-ghost-image.svg";
+    event.dataTransfer.setDragImage(ghostImage, 0, 0);
+}
+
+function ElementDragEnd(event) {
+    localStorage.removeItem("draggedElement");
 }
 
 function ElementDragEnter(event) {
@@ -34,8 +49,24 @@ function ElementDragOver(event) {
     event.preventDefault();
 }
 
-function ElementDrop(event) {
-    event.preventDefault();
-    document.getElementById(event.dataTransfer.getData("text/plain")).style.left = event.pageX + "px";
-    document.getElementById(event.dataTransfer.getData("text/plain")).style.top = event.pageY + "px";
+function ElementDrag(event) {
+    var circuitContainer = document.getElementById("circuit-container");
+    var gridSize = parseInt(getComputedStyle(circuitContainer, null).getPropertyValue("--grid-size"));
+
+    /* round to the nearest grid line and add 5 to compensate the size of the components-container */
+    var left = Math.round(event.pageX / gridSize) * gridSize + 5;
+
+    /* round to the nearest grid line and remove 8 to center and compensate the height difference between grid cells 
+    and the component */
+    var top = Math.round(event.pageY / gridSize) * gridSize - 8;
+
+    document.getElementById(localStorage.getItem("draggedElement")).style.left = left + "px";
+    document.getElementById(localStorage.getItem("draggedElement")).style.top = top + "px";
+}
+
+/* This function loads the ghost image used when draggin elements. This is necessary because otherwise the image will only
+be loaded after the first dragstart event is fired, and so will not be shown on the first drag event */
+function preloadGhostImage() {
+    var ghostImage = new Image();
+    ghostImage.src = "images/blank-ghost-image.svg";
 }
